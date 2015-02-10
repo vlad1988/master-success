4b84583c0d661dd310695d333461cc7799a468e2
Vlad <vladislafus@gmail.com>
2015-02-10 23:07:55 +0200

    Add deliver function

--- master-success.php
+++ master-success.php
@@ -12,15 +12,23 @@
 function html_form_code() {
     $form = "";
     $form.="<form action='" . esc_url($_SERVER['REQUEST_URI']) . "' method='post'>";
-    $form.="<input type='text'  placeholder='Ваше Имя'/>";
-    $form.="<input type='email'  placeholder='Ваш E-mail'/>";
-    $form.="<input type='submit' id='add-share-btn' value='Получить доступ'/>";
+    $form.="<input type='text' name='ms-name' value='".( isset( $_POST["ms-name"] ) ? esc_attr( $_POST["ms-name"] ) : '' ) ."'  placeholder='Ваше Имя'/>";
+    $form.="<input type='email' name='ms-email' value='".( isset( $_POST["ms-email"] ) ? esc_attr( $_POST["ms-email"] ) : '' )."'  placeholder='Ваш E-mail'/>";
+    $form.="<input name='ms-submit' type='submit' id='add-share-btn' value='Получить доступ'/>";
     $form.="</form>";
     echo $form;
 }
 
+function deliver_email() {
+    if (isset($_POST['ms-submit'])) {
+        $name = $_POST["ms-name"];
+        $email = $_POST["ms-email"];
+    }
+}
+
 function master_success_form() {
     html_form_code();
+    deliver_email();
 }
 
 register_activation_hook(__FILE__, 'master_success_form');
