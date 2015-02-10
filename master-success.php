a22685383d95b560eb20ea425d7cc7a66a133c42
Vlad <vladislafus@gmail.com>
2015-02-10 20:21:40 +0200

    plugin for kovalevaolga.com

---
+++ master-success.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Contact Plugin Form for Master Success Site
+Plugin URI: http://www.kovalevaolga.com/
+Description: Simple non-bloated WordPress Contact Form
+Version: 1.0
+Author: Vladyslav Tertyshnyi
+Author URI: http://brainstorage.me/vladislafus
+*/
+
