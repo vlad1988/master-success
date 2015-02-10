0bc1abaadfd866ffa08fdb5caaed0b039df31b80
Vlad <vladislafus@gmail.com>
2015-02-10 22:36:39 +0200

    Add activate hook

--- master-success.php
+++ master-success.php
@@ -1,11 +1,25 @@
 <?php
 
 /*
-Plugin Name: Contact Plugin Form for Master Success Site
-Plugin URI: http://www.kovalevaolga.com/
-Description: Simple non-bloated WordPress Contact Form
-Version: 1.0
-Author: Vladyslav Tertyshnyi
-Author URI: http://brainstorage.me/vladislafus
-*/
+  Plugin Name: Contact Plugin Form for Master Success Site
+  Plugin URI: http://www.kovalevaolga.com/
+  Description: Simple non-bloated WordPress Contact Form
+  Version: 1.0
+  Author: Vladyslav Tertyshnyi
+  Author URI: http://brainstorage.me/vladislafus
+ */
 
+function html_form_code() {
+    $form = "";
+    $form.="<form action='" . esc_url($_SERVER['REQUEST_URI']) . "' method='post'>";
+    $form.="<input type='text'  placeholder='Ваше Имя'/>";
+    $form.="<input type='email'  placeholder='Ваш E-mail'/>";
+    $form.="</form>";
+    echo $form;
+}
+
+function master_success_form() {
+    html_form_code();
+}
+
+register_activation_hook(__FILE__, 'master_success_form');
